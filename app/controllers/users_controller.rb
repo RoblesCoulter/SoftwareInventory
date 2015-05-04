e446d9387a17e1872f9733e9a6a8e9663b4a258b
Gerardo Romero <geromerohn@gmail.com>
2015-05-04 10:46:42 -0600

    Admin roles completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
   private
   	def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   	end
 
     def logged_in_user
