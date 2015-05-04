eb0eac2a6a556a2195ad100594db55a2ca6b7e9d
Luis Robles <luisrobles@HFG.local>
2015-05-04 14:00:19 -0600

    All CRUD

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,3 +1,5 @@
 class Software < ActiveRecord::Base
-	has_many :software_serials
+	has_many :software_serials, dependent: :destroy
+	validates :name, presence: true
+	validates_uniqueness_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,29 +4,15 @@ Rails.application.routes.draw do
   # get 'user/new'
 
   get 'signup' => 'users#new'
-
   resources :software_serials
-
   resources :softwares
-
   resources :movements
-
   resources :categories
-
   resources :products
-
   resources :items
-
   resources :boxes
-
   resources :users
-
   root 'welcome#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
